a1308569db2d53edeb350abb242b9a60d3a235ba
Kevin Andriessens <kevin@kelaun.be>
2020-09-09 21:39:09 +0200

    Update baraction.sh

--- spectrwm/baraction.sh
+++ spectrwm/baraction.sh
@@ -4,8 +4,8 @@
 while :; do
 
 # NETWORK
-NETSTAT="$(nmcli -p | grep wlan0 | head -1 | awk '{ print $2 }')"
-NETNAME="$(if [ $NETSTAT == "connected" ]; then echo "$(nmcli -p | grep wlan0 | head -1 | awk '{ print $4 }')"; else echo $NETSTAT; fi)"
+NETSTAT="$(nmcli -p | grep wlp1s0 | head -1 | awk '{ print $2 }')"
+NETNAME="$(if [ $NETSTAT == "connected" ]; then echo "$(nmcli -p | grep wlp1s0 | head -1 | awk '{ print $4 }')"; else echo $NETSTAT; fi)"
 
 # VOLUME
 VOLUME="$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')%"
