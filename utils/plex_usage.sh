ef6f1bcb8940f6cbdcc72256aba0a53675c9af84
Kelaun <kevin@kelaun.be>
2020-11-08 17:39:56 +0100

    Adding new mount

--- utils/plex_usage.sh
+++ utils/plex_usage.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-size_movie=$(ssh -q plex "df --output=pcent,target | grep /Media/Movies" | sed 's/% \/Media\/Movies//g' | sed 's/ //g')
+size_movie=$(ssh -q plex "df --output=pcent,target | grep /Media/Movies" | sed 's/% \/Media\/Movies//g' | sed 's/ //g' | head -1)
+size_movie2=$(ssh -q plex "df --output=pcent,target | grep /Media/Movies2" | sed 's/% \/Media\/Movies2//g' | sed 's/ //g')
 size_pictures=$(ssh -q plex "df --output=pcent,target | grep /Media/Pictures" | sed 's/% \/Media\/Pictures//g' | sed 's/ //g')
 size_tv=$(ssh -q plex "df --output=pcent,target | grep /Media/TV" | sed 's/% \/Media\/TV//g' | sed 's/ //g')
 
 if [[ $# -eq 1 ]]; then
-				echo -e "Movie is $((100-$size_movie))% free on Plex.
+				echo -e "Movies is $((100-$size_movie))% free on Plex.
+Movies 2 is $((100-$size_movie2))% free on Plex.
 Pictures is $((100-$size_pictures))% free on Plex.
 TV Shows is $((100-$size_tv))% free on Plex."
 				exit 0
@@ -14,6 +16,10 @@ fi
 if [[ $size_movie -ge 90 ]]; then
 				notify-send "Movie is $size_movie% in use on Plex."
 				
+fi
+
+if [[ $size_movie -ge 90 ]]; then
+				notify-send "Movie2 is $size_movie2% in use on Plex."
 fi
 
 if [[ $size_pictures -ge 90 ]]; then
